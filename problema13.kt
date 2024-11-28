3d1e77c721816a73aea43b14bec4a71ed858a9bd
vanenieveess <vanessaorozco131@gmail.com>
2024-11-27 23:32:18 -0600

    Delete problema13.kt

--- problema13.kt
+++
@@ -1,9 +0,0 @@
-#Escribe un programa para calcular los autovalores de una matriz 2x2.
-
-def calcular_autovalores():
-    print("Ingresa los coeficientes de la matriz 2x2 (fila por fila):")
-    matriz = [list(map(float, input().split())) for _ in range(2)]
-    matriz = np.array(matriz)
-    autovalores, autovectores = np.linalg.eig(matriz)
-    return f"Autovalores: {autovalores}\nAutovectores:\n{autovectores}"
-print(calcular_autovalores())
